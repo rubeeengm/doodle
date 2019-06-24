9ffe8f1a58272061eea09425b8685b9cd5f80a18
rubeeengm <rubeeengm@example.com>
2019-06-23 21:19:03 -0500

    tabs añadidos, con validacion en las rutas; estilos en los tabs aplicados

--- search.php
+++ search.php
@@ -1,3 +1,13 @@
+<?php
+    if (isset($_GET["term"])) {
+        $term = $_GET["term"];
+    } else {
+        exit("You must enter a search term");
+    }
+
+    $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -28,6 +38,26 @@
                             </div>
                         </form>
                     </div>
+                </div>
+
+                <div class="tabsContainer">
+                    <ul class="tabList">
+                        <li class=" <?php echo $type == 'sites' ?'active' : '' ?> ">
+                            <a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
+                                Sites
+                            </a>
+                        </li>
+
+                        <li class=" <?php echo $type == 'images' ?'active' : '' ?> ">
+                            <a href='
+                                <?php
+                                    echo "search.php?term=$term&type=images";
+                                ?>
+                            '>
+                                Images
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
