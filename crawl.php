3aee395fba14df0d172f0ab5fa7e12ec9ebaa2b0
rubeeengm <rubeeengm@gmail.com>
2019-06-24 01:15:45 -0500

    se ignoran los # y links de javascript; nueva funcion para crear links

--- crawl.php
+++ crawl.php
@@ -1,16 +1,29 @@
 <?php
     include("classes/DomDocumentParser.php");
 
+    function createLink($src, $url) {
+        echo "SRC: $src<br>";
+        echo "URL: $url<br>";
+    }
+
     function followLinks($url) {
         $parser = new DomDocumentParser($url);
         $linkList = $parser->getLinks();
 
         foreach($linkList as $link) {
             $href = $link->getAttribute("href");
-            echo $href . "<br>";
+
+            if (strpos($href, "#") !== false) {
+                continue;
+            } else if (substr($href, 0, 11) == "javascript:") {
+                continue;
+            }
+
+            createLink($href, $url);
+            //echo $href . "<br>";
         }
     }
 
-    $startUrl = "http://www.reecekenney.com";
+    $startUrl = "http://www.bbc.com";
     followLinks($startUrl);
 ?>
