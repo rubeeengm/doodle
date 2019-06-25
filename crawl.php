2ce0a81f486eb2e2fbaa955aed75f8566691c43e
rubeeengm <rubeeengm@gmail.com>
2019-06-25 02:37:52 -0500

    new logo; Recursively crawling links

--- crawl.php
+++ crawl.php
@@ -1,12 +1,32 @@
 <?php
     include("classes/DomDocumentParser.php");
 
+    $alreadyCrawled = array();
+    $crawling = array();
+
     function createLink($src, $url) {
-        echo "SRC: $src<br>";
-        echo "URL: $url<br>";
+        $scheme = parse_url($url)["scheme"]; // http
+        $host = parse_url($url)["host"]; // www.reecekenney.com
+
+        if (substr($src,0,2) == "//") {
+            $src = $scheme.":".$src;
+        } else if (substr($src,0,1) == "/") {
+            $src = $scheme."://".$host.$src;
+        } else if (substr($src,0,2) == "./") {
+            $src = $scheme."://".$host.dirname(parse_url($url)["path"]).substr($src,1);
+        } else if (substr($src,0,3) == "../") {
+            $src = $scheme."://".$host."/".$src;
+        } else if (substr($src,0,5) != "https" && substr($src,0,4) != "http") {
+            $src = $scheme."://".$host."/".$src;
+        }
+
+        return $src;
     }
 
     function followLinks($url) {
+        global $alreadyCrawled;
+        global $crawling;
+
         $parser = new DomDocumentParser($url);
         $linkList = $parser->getLinks();
 
@@ -18,9 +38,23 @@
             } else if (substr($href, 0, 11) == "javascript:") {
                 continue;
             }
+            //recupera el nuevo enlace
+            $href = createLink($href, $url);
 
-            createLink($href, $url);
-            //echo $href . "<br>";
+            if (!in_array($href, $alreadyCrawled)) {
+                $alreadyCrawled[] = $href;
+                $crawling[] = $href;
+
+                //Inser $href
+            }
+
+            echo $href . "<br>";
+        }
+
+        array_shift($crawling);
+
+        foreach($crawling as $site) {
+            followLinks($site);
         }
     }
 
